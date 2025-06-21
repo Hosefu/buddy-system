db8e99e3ac186d483c97597de1256e24771d1f31
Bug Fixer <example@example.com>
2025-06-21 19:42:34 +0300

    clear

--- packages/api/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ["@buddybot/eslint-config/base"],
-  parserOptions: {
-    project: "./tsconfig.json",
-    tsconfigRootDir: __dirname,
-  },
-}; 
--- packages/database/prisma/migrations/20250621151938_add_counts_to_flow_and_flowstep/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Flow" ADD COLUMN     "stepsCount" INTEGER NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "FlowStep" ADD COLUMN     "componentsCount" INTEGER NOT NULL DEFAULT 0;
